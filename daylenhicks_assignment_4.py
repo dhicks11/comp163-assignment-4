37980aefdb04806159048d968c8d7b86862e1284
dhicks11 <daylenhicks10@gmail.com>
2025-09-25 00:27:19 -0400

    "Pass Test Case 1: Initial game setup with required variables"

---
+++ daylenhicks_assignment_4.py
@@ -0,0 +1,12 @@
+student_name = "Daylen Hicks"
+current_gpa = 3.9
+study_hours = 19
+social_points = 60
+stress_level = 80
+
+print("Welcome to My College Life Adventure Game")
+print(f"Student: {student_name}")
+print(f"GPA: {current_gpa}")
+print(f"Study hours: {study_hours}")
+print(f"Social points: {social_points}")
+print(f"stress_level: {stress_level}")
