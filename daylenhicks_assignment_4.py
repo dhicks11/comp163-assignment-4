da78f67562336e683daa29ff09c36687874e997b
dhicks11 <daylenhicks10@gmail.com>
2025-09-26 23:03:06 -0400

    Pass Test Case 4: Complete assessment with all concepts and multiple endings

--- daylenhicks_assignment_4.py
+++ daylenhicks_assignment_4.py
@@ -63,3 +63,58 @@ elif choice not in study_options:
     print("Invalid choice, you lose social points & study hours :(")
     study_hours = study_hours - 5
     social_points = social_points - 5
+
+print("\nWAIT")
+print("There is a big party on campus tonight :)")
+print("Do you want to slide?")
+print("PS....All your friends are going")
+print("Options: Yes or No")
+
+party_choice = input("Your choice (Yes/no): ")
+
+if party_choice is not [] and party_choice is ["Yes"]:
+    print("You had a blast!")
+    social_points = social_points + 20
+    stress_level = stress_level - 10
+    if social_points > 60:
+        print("But...You partied way too hard, got drunk and lost focus for your final.")
+        current_gpa = current_gpa - 0.3
+else:
+    print("You decided to stay in and study more for your final.")
+    study_hours = study_hours + 3
+    stress_level = stress_level + 2
+
+print("\nFinal Exam Approach:")
+print("1) Study VERY HARD")
+print("2) Study lightly")
+print("3) Go over one thing and Just Wing IT!")
+
+study_choice = ("1, 2, 3")
+
+if study_choice is not None and study_choice != "":
+    if study_choice == "1":
+        if current_gpa >= 3.7:
+            print("Congratulations! You have passed with flying colors :)")
+            current_gpa = current_gpa + 0.4
+            stress_level = stress_level - 25 #passing takes a lot of stress off
+    else:
+        print("Sorry, you put in immense effort. Manage your time a little better next time.")
+        current_gpa = current_gpa + 0.1
+        stress_level = stress_level + 5
+elif study_choice == "2":
+    print("Good job, you scored pretty decently!")
+    current_gpa = current_gpa + 0.1
+    stress_level = stress_level -5
+elif study_choice == "3":
+    print("You failed, focus on your studies more next time!")
+    current_gpa = current_gpa - 0.5 #GPA penalty for poor preparation
+    stress_level = stress_level + 25
+else:
+    print("Invalid choice, Try again")
+
+# FINAL STATS DISPLAY
+print("\n=== Final Statistics ===")
+print(f"GPA: {current_gpa}")
+print(f"Study hours: {study_hours}")
+print(f"Social points: {social_points}")
+print(f"Stress Levels:  {stress_level}")
